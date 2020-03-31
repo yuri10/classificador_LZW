032ed846c59fd81055ee2a15b995e0502d47d063
yuri10 <oliveirayuri10@hotmail.com>
2020-03-31 00:03:00 -0300

    perdi tudo mas agora vai

--- classificador_lzw.py
+++ classificador_lzw.py
@@ -8,15 +8,19 @@ Created on Fri Mar 27 16:47:34 2020
 import numpy as np
 import PIL
 import matplotlib.pyplot as plt
+import time
+import random
 
 #treina com 9 imagens de cada pessoa
 #Gera o dicionario de cada pessoa e guarda numa variavel
 #
 
-def leImagensDeTreinamento(nPessoa):
-        for j in range(1,numeroImagensPessoa + 1):       
-           listaImagens.append(PIL.Image.open('C:/Users/Yuri Oliveira/Desktop/orl_faces/s'+ str(nPessoa) +'/'+ str(j) +'.pgm'))
-
+def leImagensPessoa(nPessoa):
+    listaImagens = []
+    for j in range(1,numeroImagensPessoa + 1):       
+        listaImagens.append(PIL.Image.open('C:/Users/Yuri Oliveira/Desktop/orl_faces/s'+ str(nPessoa) +'/'+ str(j) +'.pgm'))
+    return listaImagens
+    
 #transforma a imagem numa string
 def transformaImgEmString(imagem):
     mensagem = ""
@@ -62,8 +66,10 @@ def LZW(k, mensagem, dic):
     return saida
 
 listaImagens = []
-numeroPessoas = 40
+numeroPessoas = 4
 numeroImagensPessoa = 10
+kMax = 17
+
 
 '''
     Inicio do Treinamento
@@ -77,33 +83,71 @@ dicionario = [chr(i) for i in range(dic_tamanho)]
 tempos_k = []
 saidas = []   #variavel que guarda as saidas geradas por cada imagem. 40 pessoas x 9 imagens = 360 saidas
 dicionarios = []  #variavel que guarda o dicionario das 40 pessoas, um por pessoa
+listaImagensTeste = [random.randint(0,9) for i in range(numeroPessoas)]  #cria uma lista com os indices das imagens de teste de cada pessoa
+
 #faz para K valendo de 9 a 16
-for k in range(9,10):
+for k in range(9,kMax):
     
     #percorre todas as pessoas
-    for pessoa in range(1, 3+1):
-        listaImagens = []  
-        leImagensDeTreinamento(pessoa)
+    for pessoa in range(1, numeroPessoas+1):
+        listaImagens = leImagensPessoa(pessoa)
+        del listaImagens[listaImagensTeste[pessoa-1]]  #deleta a imagem que será utilizada como teste
         dic = dicionario[0:] #para cada pessoa, "zera" o dicionario
         #percorre as 9 faces de cada pessoa
-        for face in range(numeroImagensPessoa):
+        for face in range(numeroImagensPessoa-1):
             imagem = listaImagens[face]
             mensagem = transformaImgEmString(imagem)
             saida = LZW(k, mensagem, dic)
-            saidas.append(saida)
+            #saidas.append(saida)
         dicionarios.append(dic)
-            #tempos_k.append(time.time() - tempo_inicial)
+        #tempos_k.append(time.time() - tempo_inicial)
 '''            
     Fim do Treinamento
     
     Inicio da classificação
 ''' 
 
-#testando a ultima imagem de uma pessoa
-listaImagens = []
-leImagensDeTreinamento(2)
-imagem = listaImagens[9]
- 
+def classificaImagem(pessoa, k):
+    saidas = saidas_classificacao[((k-9)*numeroPessoas):((k-9)*numeroPessoas+numeroPessoas)]
+    tamanhoSaidas = [len(saida) for saida in saidas]
+    menorTamanho = min(tamanhoSaidas)
+    indice = tamanhoSaidas.index(menorTamanho)
+    if indice == (pessoa - 1):
+        return 1
+    else:
+        print("classificacao incorreta da pessoa: " + str(pessoa))
+        return -1
+    
+saidas_classificacao = []
+
+
+for k in range(9,kMax):
+    print("utilizando k: " + str(k))
+    for pessoa in range(1, numeroPessoas+1):
+        print("pessoa: " + str(pessoa))
+        listaImagens = leImagensPessoa(pessoa)
+        imagem = listaImagens[listaImagensTeste[pessoa-1]]
+        mensagem = transformaImgEmString(imagem)
+        for d in range((k-9)*numeroPessoas, ((k-9)*numeroPessoas) + numeroPessoas):
+            print("utilizando dicionario: " + str(d))
+            dic = dicionarios[d]
+            saida = LZW(k, mensagem, dic)
+            saidas_classificacao.append(saida)
+
+qtdeAcertos = []
+
+for k in range(9,kMax):
+    print("K valendo: " + str(k))
+    classificacaoAcertos = 0
+    for pessoa in range(1, numeroPessoas+1):
+        resultado = classificaImagem(pessoa, k)
+        if resultado == 1:
+            print("classificacao correta da pessoa: " + str(pessoa))
+            classificacaoAcertos += 1
+    print("Quantidade de acertos: " + str(classificacaoAcertos))
+    qtdeAcertos.append(classificacaoAcertos)
+    
+        
 #image = PIL.Image.open('C:/Users/Yuri Oliveira/Desktop/orl_faces/s1/3.pgm')
 #mostra a imagem
 #for imagem in listaImagens:
